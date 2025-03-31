9a6cd37cace5525435d59af828add6f9680a0a54
Julie-Mikoyan <jcmikoyan@kabarak.ac.ke>
2025-03-31 08:48:26 +0300

    8th commit

---
+++ verify-password.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+require 'database_connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $password = $_POST['password'];
+    $userId = $_SESSION['user_id'];
+
+
+    $stmt = $conn->prepare("SELECT password FROM users WHERE id = ?");
+    $stmt->bind_param("i", $userId);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
+    if ($user && password_verify($password, $user['password'])) {
+        echo json_encode(['success' => true]);
+    } else {
+        echo json_encode(['success' => false]);
+    }
+
+    $stmt->close();
+} else {
+    echo json_encode(['success' => false]);
+}
+
+$conn->close();
+?>
